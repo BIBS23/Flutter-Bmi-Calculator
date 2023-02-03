4377bb98b8c9f67b02226847ef239ddd7b4941d1
BIBS23 <bibinoottukulathil@gmail.com>
2023-02-03 19:00:58 +0530

    pushed

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,7 @@
 import 'package:bmicalculator/utils/bmi_meter.dart';
 import 'package:bmicalculator/utils/measurements_cards.dart';
-import 'package:audioplayers/audioplayers.dart';
-import 'package:flutter/services.dart';
-import 'dart:typed_data';
+import 'package:lottie/lottie.dart';
+import 'package:flutter_circular_text/circular_text.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -12,11 +11,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  String audioasset = "lib/assets/buttontune.mp3";
-
   String val = 0.toString();
   var bmi = '';
+  bool isbtnclicked = false;
   Widget build(BuildContext context) {
+    @override
     void calc(double height, double weight) {
       double bmival = weight / (height * height / 10000);
       setState(() {
@@ -44,13 +43,9 @@ class _HomeState extends State<Home> {
       }
     }
 
-    String audioasset = "assets/audio/red-indian-music.mp3";
-
-    AudioPlayer player = AudioPlayer();
-
-    @override
     TextEditingController heightinput = TextEditingController();
     TextEditingController weightinput = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -95,55 +90,73 @@ class _HomeState extends State<Home> {
             ),
             const SizedBox(height: 40),
             SizedBox(
-              height: 50,
-              width: 120,
-              child: ElevatedButton(
-                  onPressed: () async {
-                    double h = double.parse(heightinput.text);
-                    double w = double.parse(weightinput.text);
-                    calc(h, w);
-                    result();
-                    final snackBar = SnackBar(
-                      backgroundColor: Colors.pinkAccent,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      content: SizedBox(
-                        height: 250,
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(
-                                bmi,
-                                style: const TextStyle(
-                                    overflow: TextOverflow.clip,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
+                height: 50,
+                width: 120,
+                child: ElevatedButton(
+                    onPressed: () {
+                      double h = double.parse(heightinput.text);
+                      double w = double.parse(weightinput.text);
+                      calc(h, w);
+                      result();
+
+                      if (heightinput.text.isEmpty ||
+                          weightinput.text.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Enter a valid input')));
+                      } else {
+                        final snackBar = SnackBar(
+                          backgroundColor: Colors.pinkAccent,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          content: SizedBox(
+                            height: 250,
+                            child: Center(
+                              child: SingleChildScrollView(
+                                child: Stack(
+                                  alignment: Alignment.center,
+                                  children: [
+                                    Lottie.network(
+                                        'https://assets1.lottiefiles.com/packages/lf20_2m1smtya.json'),
+                                    Positioned(
+                                      top: 100,
+                                      child: Text(
+                                        bmi,
+                                        style: const TextStyle(
+                                            overflow: TextOverflow.clip,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    Positioned(
+                                      top: 140,
+                                      child: Text(
+                                        'BMI Range : $val',
+                                        style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
-                              Text(
-                                'BMI Range : $val',
-                                style: const TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.bold),
-                              ),
-                            ],
+                            ),
                           ),
-                        ),
-                      ),
-                      duration: const Duration(seconds: 2),
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  },
-                  style: ButtonStyle(
-                      shadowColor: const MaterialStatePropertyAll(Colors.pink),
-                      backgroundColor:
-                          const MaterialStatePropertyAll(Colors.white),
-                      shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(100)))),
-                  child: Text('Calculate',
-                      style: TextStyle(
-                          fontSize: 13, color: Colors.grey.shade600))),
-            ),
+                          duration: const Duration(seconds: 3),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      }
+                    },
+                    style: ButtonStyle(
+                        shadowColor:
+                            const MaterialStatePropertyAll(Colors.pink),
+                        backgroundColor:
+                            const MaterialStatePropertyAll(Colors.white),
+                        shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100)))),
+                    child: Text('Calculate',
+                        style: TextStyle(
+                            fontSize: 13, color: Colors.grey.shade600)))),
           ],
         ),
       ),
--- lib/utils/bmi_meter.dart
+++ lib/utils/bmi_meter.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:simple_gradient_text/simple_gradient_text.dart';
+import 'package:flutter_circular_text/circular_text.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class BmiMeter extends StatelessWidget {
@@ -8,79 +10,90 @@ class BmiMeter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 300,
-        width: 300,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(150),
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.pink.withOpacity(0.5),
-                spreadRadius: 1,
-                blurRadius: 5,
-              ),
-            ]),
-        child: SfRadialGauge(axes: <RadialAxis>[
-          RadialAxis(minimum: 0, maximum: 40, ranges: <GaugeRange>[
-            GaugeRange(
-              startWidth: 23,
-              endWidth: 23,
-              startValue: 0,
-              endValue: 18.5,
-              gradient: SweepGradient(colors: [
-                Colors.red.shade400,
-                Colors.blue.shade400,
-              ]),
-              label: 'Underweight',
-            ),
-            GaugeRange(
-              startWidth: 23,
-              endWidth: 23,
-              startValue: 18.5,
-              endValue: 24.9,
-              color: Colors.pink.shade300,
-              label: 'Normal',
-            ),
-            GaugeRange(
-              startWidth: 23,
-              endWidth: 23,
-              startValue: 25,
-              endValue: 29.9,
-              color: Colors.deepPurple.shade300,
-              label: 'Overweight',
-            ),
-            GaugeRange(
-                startWidth: 23,
-                endWidth: 23,
-                startValue: 30,
-                endValue: 40,
-                gradient: SweepGradient(colors: [
-                  Colors.blue.shade400,
-                  Colors.red.shade400,
+    return Stack(children: [
+      Lottie.network(
+          'https://assets1.lottiefiles.com/packages/lf20_agxjuzgt.json',
+          width: 350,
+          height: 350),
+      Positioned(
+        left: 25,
+        top: 25,
+        child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(150),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.pink.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                  ),
                 ]),
-                label: 'Obesity',
-                labelStyle: const GaugeTextStyle())
-          ], pointers: <GaugePointer>[
-            NeedlePointer(
-              enableAnimation: true,
-              animationType: AnimationType.easeInCirc,
-              value: double.parse(value),
-              needleStartWidth: 3,
-              needleColor: Colors.pinkAccent,
-              knobStyle:
-                  const KnobStyle(knobRadius: 0.170, color: Colors.white),
-            ),
-          ], annotations: <GaugeAnnotation>[
-            GaugeAnnotation(
-              angle: 85,
-              positionFactor: 0.8,
-              widget: GradientText(value,
-                  style: const TextStyle(
-                      fontSize: 30, fontWeight: FontWeight.bold),
-                  colors: const [Colors.red, Colors.blue]),
-            ),
-          ]),
-        ]));
+            child: SfRadialGauge(axes: <RadialAxis>[
+              RadialAxis(minimum: 0, maximum: 40, ranges: <GaugeRange>[
+                GaugeRange(
+                  startWidth: 23,
+                  endWidth: 23,
+                  startValue: 0,
+                  endValue: 18.5,
+                  gradient: SweepGradient(colors: [
+                    Colors.red.shade400,
+                    Colors.blue.shade400,
+                  ]),
+                  label: 'Underweight',
+                ),
+                GaugeRange(
+                  startWidth: 23,
+                  endWidth: 23,
+                  startValue: 18.5,
+                  endValue: 24.9,
+                  color: Colors.pink.shade300,
+                  label: 'Normal',
+                  
+                ),
+                GaugeRange(
+                  startWidth: 23,
+                  endWidth: 23,
+                  startValue: 25,
+                  endValue: 29.9,
+                  color: Colors.deepPurple.shade300,
+                  label: 'Overweight',
+                ),
+                GaugeRange(
+                    startWidth: 23,
+                    endWidth: 23,
+                    startValue: 30,
+                    endValue: 40,
+                    gradient: SweepGradient(colors: [
+                      Colors.blue.shade400,
+                      Colors.red.shade400,
+                    ]),
+                    label: 'Obesity',
+                    labelStyle: const GaugeTextStyle())
+              ], pointers: <GaugePointer>[
+                NeedlePointer(
+                  enableAnimation: true,
+                  animationType: AnimationType.easeInCirc,
+                  value: double.parse(value),
+                  needleStartWidth: 3,
+                  needleColor: Colors.pinkAccent,
+                  knobStyle:
+                      const KnobStyle(knobRadius: 0.170, color: Colors.white),
+                ),
+              ], annotations: <GaugeAnnotation>[
+                GaugeAnnotation(
+                  angle: 85,
+                  positionFactor: 0.8,
+                  widget: GradientText(value,
+                      style: const TextStyle(
+                          fontSize: 30, fontWeight: FontWeight.bold),
+                      colors: const [Colors.red, Colors.blue]),
+                ),
+              ]),
+            ])),
+      ),
+    ]);
   }
 }
